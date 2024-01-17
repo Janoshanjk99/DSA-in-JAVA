a0458ccb108fd7ac2da058574b20cc247439abc0
Jeyarasa Janoshan <134253962+Janoshanjk99@users.noreply.github.com>
2024-01-17 23:45:36 +0530

    Janoshan

---
+++ DSA in JAVA/src/Array/insert_an_element.java
@@ -0,0 +1,48 @@
+package Array;
+import java.util.Scanner;
+/**
+ *
+ * @author JANOSH JK
+ */
+public class insert_an_element {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.print("Enter the position to insert the element: ");
+        int position = scanner.nextInt();
+
+        System.out.print("Enter the element to insert: ");
+        int element = scanner.nextInt();
+
+        if (position >= 1 && position <= size + 1) {
+            int[] newArray = new int[size + 1];
+
+            for (int i = 0, j = 0; i < newArray.length; i++) {
+                if (i == position - 1) {
+                    newArray[i] = element;
+                } else {
+                    newArray[i] = array[j];
+                    j++;
+                }
+            }
+            System.out.println("Array after insertion to specific position:");
+            for (int value : newArray) {
+                System.out.print(value + " ");
+            }
+        } else {
+            System.out.println("Invalid position. Please enter a valid position.");
+        }
+
+        scanner.close();
+    }
+}
