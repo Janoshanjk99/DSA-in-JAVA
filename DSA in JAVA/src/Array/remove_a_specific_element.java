e43b07088ef62413a0a44334f897e8867def1883
Jeyarasa Janoshan <134253962+Janoshanjk99@users.noreply.github.com>
2024-01-17 23:53:14 +0530

    Janoshan

--- DSA in JAVA/src/Array/remove_a_specific_element.java
+++ DSA in JAVA/src/Array/remove_a_specific_element.java
@@ -1,1 +1,49 @@
+package Array;
+import java.util.Scanner;
+/**
+ *
+ * @author JANOSH JK
+ */
+public class remove_a_specific_element {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.print("Enter the element to remove: ");
+        int elementToRemove = scanner.nextInt();
+
+        boolean elementFound = false;
+        int newSize = 0;
+
+        for (int i = 0; i < size; i++) {
+            if (array[i] == elementToRemove) {
+                elementFound = true;
+            } else {
+                array[newSize++] = array[i];
+            }
+        }
+
+        if (elementFound) {
+            System.out.println("Element removed successfully.");
+
+            System.out.println("Array after removal:");
+            for (int i = 0; i < newSize; i++) {
+                System.out.print(array[i] + " ");
+            }
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+
+        scanner.close();
+    }
+}
 
